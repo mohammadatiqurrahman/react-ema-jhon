fa04f4e2aedc834f65f7a2addd3f25672ca5c304
Mohammad Atiqur Rahman <atiqur15-6983@diu.edu.bd>
2021-09-24 00:09:15 +0600

    update order summary

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
     let total = 0;
     for(const product of props.cart){
         total = total+product.price;
     }
+    let shipping = 0;
+    for(const shippingg of props.cart){
+        shipping = shipping+shippingg.shipping;
+    }
     return (
         <div>
-            <h4>Order Summary</h4>
+            <div className="order-summary">
+            <h3>Order Summary</h3>
             <p>Order Items: {props.cart.length}</p>
-            <br />
-            <h4>Total Price: ${total.toFixed(2)}</h4>
+            </div>
+            <div>
+            <table>
+                <tr>
+                    <td>Shipping & Handling: </td>
+                    <td> ${shipping.toFixed(2)}</td>
+                </tr>
+                <tr>
+                    <td>Total Before Tax: </td>
+                    <td>$ {total.toFixed(2)}</td>
+                </tr>
+                <tr>
+                    <td><h3>Order Total: </h3></td>
+                    <td><h3>$ {(total+shipping).toFixed(2)}</h3></td>
+                </tr>
+            </table>
+            </div>
         </div>
     );
 };
